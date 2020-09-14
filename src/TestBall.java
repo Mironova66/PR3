956ed754c3a3c5ee40fa70cd1d1902c8c483b919
Katerina <katyusha.agapova@yandex.ru>
2020-09-14 20:29:17 +0300

    Initial commit

---
+++ src/TestBall.java
@@ -0,0 +1,8 @@
+public class TestBall {
+    public static void main(String[] args) {
+        Ball b1 = new Ball(50, 50);
+        System.out.println(b1);
+        b1.move(16, 8);
+        System.out.println(b1);
+    }
+}
